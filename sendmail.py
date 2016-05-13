ad60476d330baf677ae7f425cdc8afcf1a936502
linuxu2015 <linuxu2015@gmail.com>
2016-05-13 17:18:29 +0800

    first commit

---
+++ sendmail.py
@@ -0,0 +1,34 @@
+#coding:utf-8
+from email import encoders
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr, formataddr
+import smtplib
+
+def _format_addr(s):
+    name, addr = parseaddr(s)
+    return formataddr(( \
+        Header(name, 'utf-8').encode(), \
+        addr.encode('utf-8') if isinstance(addr, unicode) else addr))
+def send_mail(tos,subject,content):
+    user = 'xulibao@everyoo.com'
+    password = 'Aioute@2015'
+    #from_addr = '11111@qq.com'
+    smtp_server = 'smtp.aioute.com'
+    #to_addr = '371044414@qq.com'
+    #from_addr邮件列表显示标题
+    #subject 邮件标题
+    #to_addr 接受者
+
+    msg = MIMEText('%s' %content, 'html', 'utf-8')
+    msg['From'] = _format_addr(u'报警')
+    msg['To'] = _format_addr(u'%s' % tos)
+    msg['Subject'] = Header(u'%s','utf-8' ).encode() %subject
+
+    server = smtplib.SMTP(smtp_server, 25)
+    #server.set_debuglevel(1)
+    server.login(user, password)
+    server.sendmail(user, [tos], msg.as_string())
+    server.quit()
+#send_mail('371044414@qq.com','hahahahahah')
+#
---
+++ wsgi.py
@@ -0,0 +1,56 @@
+#coding:utf-8
+from flask import Flask
+from flask import jsonify
+from flask import request
+from sendmail import send_mail
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+app = Flask(__name__)
+
+'''@app.route('/')
+def hello_world():
+    return '<h1>Hello World!</h1>'
+    '''
+status = [
+    {
+        'status': 200,
+        'info': '发送成功'
+    }
+]
+@app.route('/', methods=['GET', 'POST'])
+def test():
+    if request.method == 'POST':
+        tos = request.form.get('tos')
+        content = request.form.get('content')
+        subject = request.form.get('subject')
+        #print 'content: %s' %content
+        data = content.split('\n')
+        if data[0] == 'OK':
+            alarm_status = '<b style="color:green;">%s</b>' % data[0]
+        else:
+            alarm_status = '<b style="color:red;">%s</b>' % data[0]
+        new_content = '''
+报警状态：%s<br>
+报警级别：%s<br>
+故障主机：%s<br>
+监控项：%s<br>
+标记:%s<br>
+出发条件：%s<br>
+备注：%s<br>
+最大次数：%s<br>
+当前次数：%s<br>
+报警时间：%s<br>
+相关策略：%s<br>
+''' % (alarm_status,data[1],data[2].split(':')[1],data[3].split(':')[1],data[4].split(':')[1],data[5],data[6].split(':')[1],data[7].split(':')[1].split(',')[0],data[7].split(':')[2],data[8].split(':')[1]+data[8].split(':')[2]+data[8].split(':')[3],data[9]) 
+        send_mail(tos,subject,new_content)
+        return jsonify({'resp':status})
+    elif request.method == 'GET':
+        return jsonify({'resp':status})
+    else:
+        return 'no data'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5555,debug=True)
