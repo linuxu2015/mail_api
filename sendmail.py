6c738f3d667e6c5ee2691b628f72e75ad52e1525
xulibao <xulibal@aioute.com>
2017-05-12 10:14:21 +0800

    针对falcon-plus做修改，目前只支持单个主机的报警

--- sendmail.py
+++ sendmail.py
@@ -11,8 +11,8 @@ def _format_addr(s):
         Header(name, 'utf-8').encode(), \
         addr.encode('utf-8') if isinstance(addr, unicode) else addr))
 def send_mail(tos,subject,content):
-    user = 'xulibao@everyoo.com'
-    password = 'Aioute@2015'
+    user = 'xulibao@aioute.com'
+    password = '1qaz2wsx'
     #from_addr = '11111@qq.com'
     smtp_server = 'smtp.aioute.com'
     #to_addr = '371044414@qq.com'
@@ -21,14 +21,14 @@ def send_mail(tos,subject,content):
     #to_addr 接受者
 
     msg = MIMEText('%s' %content, 'html', 'utf-8')
-    msg['From'] = _format_addr(u'报警')
-    msg['To'] = _format_addr(u'%s' % tos)
-    msg['Subject'] = Header(u'%s','utf-8' ).encode() %subject
+    msg['From'] = (u'falcon报警')
+    msg['To'] = (u'%s' % tos)
+    msg['Subject'] = subject
 
     server = smtplib.SMTP(smtp_server, 25)
     #server.set_debuglevel(1)
     server.login(user, password)
     server.sendmail(user, [tos], msg.as_string())
     server.quit()
-#send_mail('371044414@qq.com','hahahahahah')
+#send_mail('371044414@qq.com','hahahahahah','dd')
 #
--- wsgi.py
+++ wsgi.py
@@ -3,9 +3,9 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from sendmail import send_mail
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
+#import sys
+#reload(sys)
+#sys.setdefaultencoding('utf-8')
 
 app = Flask(__name__)
 
@@ -25,13 +25,16 @@ def test():
         tos = request.form.get('tos')
         content = request.form.get('content')
         subject = request.form.get('subject')
-        #print 'content: %s' %content
+        print('content: %s' %content)
         data = content.split('\n')
+       # print('################')
+       # print(data)
         if data[0] == 'OK':
             alarm_status = '<b style="color:green;">%s</b>' % data[0]
         else:
             alarm_status = '<b style="color:red;">%s</b>' % data[0]
         new_content = '''
+<a href=http://192.168.100.145:8081/portal/alarm-dash/case>报警面板</a>------<a href=%s>策略</a><br>
 报警状态：%s<br>
 报警级别：%s<br>
 故障主机：%s<br>
@@ -42,15 +45,13 @@ def test():
 最大次数：%s<br>
 当前次数：%s<br>
 报警时间：%s<br>
-相关策略：%s<br>
-''' % (alarm_status,data[1],data[2].split(':')[1],data[3].split(':')[1],data[4].split(':')[1],data[5],data[6].split(':')[1],data[7].split(':')[1].split(',')[0],data[7].split(':')[2],data[8].split(':')[1]+data[8].split(':')[2]+data[8].split(':')[3],data[9]) 
+''' % (data[9].replace('127.0.0.1','192.168.100.145'),alarm_status,data[1],data[2].split(':')[1],data[3].split(':')[1],data[4].split(':')[1],data[5],data[6].split(':')[1],data[7].split(':')[1].split(',')[0],data[7].split(':')[2],data[8].split(':')[1]+data[8].split(':')[2]+data[8].split(':')[3])
         send_mail(tos,subject,new_content)
         return jsonify({'resp':status})
     elif request.method == 'GET':
         return jsonify({'resp':status})
     else:
         return 'no data'
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5555,debug=True)
