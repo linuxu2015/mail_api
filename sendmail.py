85e0daf8db32f39b9bdccdd68f85c108e374ceed
xulibao <xulibal@aioute.com>
2017-05-12 10:25:51 +0800

    针对falcon-plus做修改，目前只支持单个主机的报警

--- sendmail.py
+++ sendmail.py
@@ -5,17 +5,11 @@ from email.mime.text import MIMEText
 from email.utils import parseaddr, formataddr
 import smtplib
 
-def _format_addr(s):
-    name, addr = parseaddr(s)
-    return formataddr(( \
-        Header(name, 'utf-8').encode(), \
-        addr.encode('utf-8') if isinstance(addr, unicode) else addr))
+
 def send_mail(tos,subject,content):
-    user = 'xulibao@aioute.com'
-    password = '1qaz2wsx'
-    #from_addr = '11111@qq.com'
-    smtp_server = 'smtp.aioute.com'
-    #to_addr = '371044414@qq.com'
+    user = ''
+    password = ''
+    smtp_server = ''
     #from_addr邮件列表显示标题
     #subject 邮件标题
     #to_addr 接受者
@@ -30,5 +24,3 @@ def send_mail(tos,subject,content):
     server.login(user, password)
     server.sendmail(user, [tos], msg.as_string())
     server.quit()
-#send_mail('371044414@qq.com','hahahahahah','dd')
-#
